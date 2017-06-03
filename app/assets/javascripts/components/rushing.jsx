6e4a1d40ab6487f8e26d62ac1db373e8fdf11264
nficca <nicficca@gmail.com>
2017-06-03 17:51:39 -0400

    Fix some missing columns issue

--- app/assets/javascripts/components/rushing.jsx
+++ app/assets/javascripts/components/rushing.jsx
@@ -6,13 +6,14 @@ class Rushing extends React.Component {
                 <td>{this.props.rushing.team}</td>
                 <td>{this.props.rushing.position}</td>
                 <td>{this.props.rushing.attempts_per_game}</td>
+                <td>{this.props.rushing.attempts}</td>
                 <td>{this.props.rushing.yards}</td>
                 <td>{this.props.rushing.average_yards_per_attempt}</td>
                 <td>{this.props.rushing.yards_per_game}</td>
                 <td>{this.props.rushing.touchdowns}</td>
                 <td>{this.props.rushing.longest_rush}</td>
                 <td>{this.props.rushing.first_downs}</td>
-                <td>{this.props.rushing.first_downs_percentage}</td>
+                <td>{this.props.rushing.first_down_percentage}</td>
                 <td>{this.props.rushing.twenty_yards_each}</td>
                 <td>{this.props.rushing.forty_yards_each}</td>
                 <td>{this.props.rushing.fumbles}</td>
