8bc88601a1c8023393cf3365a0734bebf1852eb8
nficca <nicficca@gmail.com>
2017-06-04 14:18:28 -0400

    Make table sortable

--- app/assets/javascripts/components/rushings.jsx
+++ app/assets/javascripts/components/rushings.jsx
@@ -1,18 +1,40 @@
 class Rushings extends React.Component {
     constructor(props) {
-        super(props)
-    }
+        super(props);
+        this.state = {
+            data: props.data,
+            sort: { column: 'id', asc: true }
+        };
 
-    static get defaultProps() {
-        return {
-            rushings: []
-        }
+        this.sortChevron = this.sortChevron.bind(this);
     }
     
     render() {
-        rows = [];
-        for (let rushing of this.props.data) {
-            rows.push(<Rushing rushing={rushing}/>);
+        let rows = [],
+            headers = [];
+
+        // Add each header with a button for sorting by that column
+        for (let stat in this.props.stats) {
+            let button_text = this.props.stats[stat];
+            let chevron = <span className="chevron">{this.sortChevron(stat)}</span>;
+            
+            headers.push(
+                <th key={stat}>
+                    <a className="sort-button" onClick={this.sortBy.bind(this, stat)}>
+                        {button_text}{chevron}
+                    </a>
+                </th>
+            );
+        }
+
+        // Create a row for each Rushing entry
+        for (let i = 0; i < this.state.data.length; ++i) {
+            rows.push(<Rushing 
+                key={this.state.data[i].id} 
+                data={this.state.data[i]} 
+                stats={this.props.stats} 
+                even={i % 2 === 0}/>
+            );
         }
 
         return (
@@ -20,21 +42,7 @@ class Rushings extends React.Component {
                 <h2 className="title">Rushings</h2>
                 <table className="table table-bordered">
                     <thead><tr>
-                        <th>Player</th>
-                        <th>Team</th>
-                        <th>Pos</th>
-                        <th>Att/G</th>
-                        <th>Att</th>
-                        <th>Yrds</th>
-                        <th>Avg</th>
-                        <th>Yds/G</th>
-                        <th>TD</th>
-                        <th>Lng</th>
-                        <th>1st</th>
-                        <th>1st%</th>
-                        <th>20+</th>
-                        <th>40+</th>
-                        <th>FUM</th>
+                        {headers}
                     </tr></thead>
                     <tbody>
                         {rows}
@@ -42,5 +50,55 @@ class Rushings extends React.Component {
                 </table>
             </div>
         );
+    }
+
+    // Sorts the data by the given column
+    // 1st click is ASC, 2nd is DESC, 3rd stops sorting by the column
+    sortBy(column) {
+        this.setState(prevState => {
+
+            // 1st click -> ascending sort
+            if (prevState.sort.column != column) {
+                prevState.sort.column = column;
+                prevState.sort.asc = true;
+            } 
+            // 2nd click -> descending sort
+            else if (prevState.sort.asc == true) {
+                prevState.sort.asc = false;
+            } 
+            // 3rd click -> revert to default sort (id)
+            else {
+                prevState.sort.column = 'id';
+                prevState.sort.asc = true;
+            }
+
+            column = prevState.sort.column;
+
+            // Sort the data by the given column
+            prevState.data.sort((a, b) => {
+                if (a[column] < b[column]) {
+                    return prevState.sort.asc ? -1 : 1;
+                } else if (a[column] > b[column]) {
+                    return prevState.sort.asc ? 1 : -1;
+                } 
+                // Tiebreaker sort is ID
+                else if (a.id < b.id) {
+                    return prevState.sort.asc ? -1 : 1;
+                } else if (a.id > b.id) {
+                    return prevState.sort.asc ? 1 : -1;
+                }
+                return 0;
+            });
+            
+            return prevState;
+        });
+    }
+
+    // Returns ▲, ▼, or empty string depending on how the column is being sorted
+    sortChevron(column) {
+        if (this.state.sort.column == column) {
+            return '\u00A0' + (this.state.sort.asc ? '\u25B2' : '\u25BC');
+        }
+        return '';
     }
 }
--- app/controllers/rushings_controller.rb
+++ app/controllers/rushings_controller.rb
@@ -1,5 +1,28 @@
 class RushingsController < ApplicationController
     def index
         @rushings = Rushing.all
+        @stats = stat_names
+    end
+
+    private
+
+    def stat_names
+        {
+            player: "Player",
+            team: "Team",
+            position: "Pos",
+            attempts: "Att",
+            attempts_per_game: "Att/G",
+            yards: "Yds",
+            average_yards_per_attempt: "Avg",
+            yards_per_game: "Yds/G",
+            touchdowns: "TD",
+            longest_rush: "Lng",
+            first_downs: "1st",
+            first_down_percentage: "1st%",
+            twenty_yards_each: "20+",
+            forty_yards_each: "40+",
+            fumbles: "FUM"
+        }
     end
 end
