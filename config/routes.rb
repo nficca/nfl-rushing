7751f2757942d3eb6f1e33f3ec04e7465f82b549
nficca <nicficca@gmail.com>
2017-06-04 18:33:13 -0400

    Restricted rushings route to index and made it the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :rushings
+  root 'rushings#index'
+
+  resources :rushings, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
